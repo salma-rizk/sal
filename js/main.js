0505b620a592ede29f37bf7fc63e9682f40ffe0a
salma-rizk <85620083+salma-rizk@users.noreply.github.com>
2021-06-09 13:16:17 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var ayHaga = prompt("heyyoo! wazyo name? plese provide it here: "); 
+
+console.log(ayHaga);
+
+document.getElementById("showayHaga").innerText = ayHaga; 
+
+// tooltip or
+
+/* tooltip */ 
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
